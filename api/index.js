d9f570c3f0c1202167f12c05a5c667901a0ae1a7
Abdifatah Yusuf <studentabdifatah@gmail.com>
2024-02-27 18:03:20 +0300

    connect mongodb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,23 @@
 const express = require("express");
+const colors = require("colors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB".yellow);
+  })
+  .catch((err) => {
+    console.log(`Error: ${err.message} `.red);
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`.blue);
 });
