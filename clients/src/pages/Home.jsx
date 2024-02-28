22a438c0fb934e511c0147ed3b9a99f48e041a5a
Abdifatah Yusuf <studentabdifatah@gmail.com>
2024-02-28 19:16:54 +0300

    ddd

--- clients/src/pages/Home.jsx
+++ clients/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home Page jjwkddjhdddjklhdkljh skdjhcskljdhs</div>;
+  return <div>Home Page jjwkddjhdddjdddklhdkljh skdjhcskljdhs</div>;
 };
 
 export default Home;
