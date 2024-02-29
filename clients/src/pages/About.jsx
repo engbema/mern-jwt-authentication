c3f70cb0fec5d135131f913192cd25e8182916bc
Abdifatah Yusuf <studentabdifatah@gmail.com>
2024-02-29 20:07:55 +0300

    a

--- clients/src/pages/About.jsx
+++ clients/src/pages/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const About = () => {
-  return <div>About Paaaaaaaaaaaaagffadde jdjhdjkhlddskjhshkjg </div>;
+  return <div>About Paaaaaaaaaaaaaagffadde jdjhdjkhlddskjhshkjg </div>;
 };
 
 export default About;
