4d3f9c4ab2c9154ac256dac997929017792059b9
Abdifatah Yusuf <studentabdifatah@gmail.com>
2024-02-29 17:31:41 +0300

    create signin api route

--- api/controller/auth.js
+++ api/controller/auth.js
@@ -1,6 +1,8 @@
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const { errorHandler } = require("../utils/error");
+
 exports.signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = await bcrypt.hashSync(password, 10);
@@ -12,6 +14,26 @@ exports.signup = async (req, res, next) => {
   try {
     const user = await newUser.save();
     res.status(201).json(user);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "Invalid Credentials!"));
+    const isMatch = await bcrypt.compare(password, validUser.password);
+    if (!isMatch) return next(errorHandler(401, "Wrong Credentials!"));
+    const token = jwt.sign({ _id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    // expiryDate
+    const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+    res
+      .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json(rest);
   } catch (err) {
     next(err);
   }
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { signup } = require("../controller/auth");
+const { signup, signIn } = require("../controller/auth");
 
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/signin", signIn);
 
 module.exports = router;
