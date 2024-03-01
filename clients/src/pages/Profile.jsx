d839727c4c5dc7ea55f57a045fecf0d8c1565dea
Abdifatah Yusuf <studentabdifatah@gmail.com>
2024-03-01 09:39:29 +0300

    update profile page ui

--- clients/src/pages/Profile.jsx
+++ clients/src/pages/Profile.jsx
@@ -1,7 +1,56 @@
-import React from "react";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
-  return <div>Profile Paddge akjhdkjsddhd,jhsdsssds</div>;
+  const { currentUser, loading, error } = useSelector((state) => state.user);
+
+  console.log(currentUser);
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <img
+          src={
+            currentUser.profilePicture
+              ? currentUser.profilePicture
+              : "https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg"
+          }
+          alt="profile"
+          className="h-24 w-24 self-center mt-2 cursor-pointer rounded-full object-cover"
+        />
+        <input
+          defaultValue={currentUser.username}
+          type="text"
+          id="username"
+          placeholder="Username"
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+        <input
+          defaultValue={currentUser.email}
+          type="email"
+          id="email"
+          placeholder="Email"
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+        <input
+          type="password"
+          id="password"
+          placeholder="Password"
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Update"}
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete Acount</span>
+        <span className="text-red-700 cursor-pointer">SignOut</span>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
